c4dbe483ed8bf49a791a1e838bccb98f1332b5e4
Vladimir Muraito <vl.muraito@mail.ru>
2024-06-17 19:57:52 +1000

    Initial commit.

---
+++ src/main/java/HM2_4/calculator/muraito/CalculatorApplication.java
@@ -0,0 +1,13 @@
+package HM2_4.calculator.muraito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorApplication.class, args);
+	}
+
+}
